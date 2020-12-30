d2b6b8828cd786ffe8b3e0e5c68f3696b82a7caf
timerke <timerke@mail.ru>
2020-12-30 19:55:42 +0300

    Выполнил задание к уроку 3

---
+++ Lesson3/models.py
@@ -0,0 +1,95 @@
+"""Модуль содержит модели таблиц из базы данных."""
+
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import backref, relationship
+
+
+Base = declarative_base()
+
+
+class Author(Base):
+    """Модель для таблицы с авторами постов."""
+
+    __tablename__ = 'author'
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    name = Column(String, nullable=False)
+    url = Column(String, nullable=False, unique=True)
+
+    def __init__(self, name, url):
+        self.name = name
+        self.url = url
+
+    def __repr__(self):
+        return f'<Author({self.name}, {self.url})>'
+
+
+class Post(Base):
+    """Модель для таблицы с постами."""
+
+    __tablename__ = 'post'
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    url = Column(String, nullable=False, unique=True)
+    title = Column(String, nullable=False)
+    picture = Column(String, nullable=True)
+    date = Column(String, nullable=False)
+    author_id = Column(Integer, ForeignKey('author.id'))
+    author = relationship(Author, backref=backref('posts', uselist=True))
+    #keywords = relationship('keyword', secondary='post_keyword_link')
+
+    def __init__(self, url, title, picture, date, author):
+        self.url = url
+        self.title = title
+        self.picture = picture
+        self.date = date
+        self.author = author
+
+    def __repr__(self):
+        return f'<Post({self.title}, {self.date})>'
+
+
+class Keyword(Base):
+    """Модель для таблицы с ключевыми словами."""
+
+    __tablename__ = 'keyword'
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    name = Column(String, nullable=False, unique=True)
+    posts = relationship(Post, secondary='post_keyword_link')
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return f'<Keyword({self.name})>'
+
+
+class PostKeywordLink(Base):
+    """Модель для осуществления связи многие-к-многим между моделями Post и
+    Keyword."""
+
+    __tablename__ = 'post_keyword_link'
+    post_id = Column(Integer, ForeignKey('post.id'), primary_key=True)
+    keyword_id = Column(Integer, ForeignKey('keyword.id'), primary_key=True)
+    post = relationship(Post, backref=backref('post_assoc'))
+    keyword = relationship(Keyword, backref=backref('keyword_assoc'))
+
+
+class Comment(Base):
+    """Модель для таблицы с комментариями."""
+
+    __tablename__ = 'comment'
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    author = Column(String, nullable=False)
+    author_url = Column(String, nullable=False)
+    text = Column(String)
+    post_id = Column(Integer, ForeignKey('post.id'))
+    post = relationship(Post, backref=backref('posts', uselist=True))
+
+    def __init__(self, author, author_url, text, post):
+        self.author = author
+        self.author_url = author_url
+        self.text = text
+        self.post = post
+
+    def __repr__(self):
+        return f'<Keyword({self.author}, {self.text})>'
